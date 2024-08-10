60f692a860475216079239c97229620374b1e6fd
huuthang106 <thangcachep106@gmail.com>
2024-08-10 23:49:51 +0700

    [update]-bổ-sung-giao-diện

--- DATN_FALL2024_WebsiteTimTro/app/Http/Controllers/Client/IndexClientController.php
+++ DATN_FALL2024_WebsiteTimTro/app/Http/Controllers/Client/IndexClientController.php
@@ -8,4 +8,7 @@ use Illuminate\Http\Request;
 class IndexClientController extends Controller
 {
     //
+    public function index(){
+        // return view('');
+    }
 }
---
+++ DATN_FALL2024_WebsiteTimTro/resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @stack('styleUs')
+    <!-- Fonts -->
+
+
+</head>
+
+<body>
+    @if (Request::is('/'))
+        <x-navbar-home />
+    @else
+        <x-navbar-default />
+    @endif
+    @yield('contentUs')
+</body>
+@stack('scriptUs')
+
+</html>
